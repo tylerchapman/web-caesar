7c5851461ed11733fc942159d8ca29e8bfbd3a69
chapmant <tylerchapman90@gmail.com>
2017-09-25 17:59:24 -0500

    finished project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from caesar input rotate_string
+from flask import Flask, render_template, request
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -8,5 +8,16 @@ app.config['DEBUG'] = True
 @app.route("/index")
 def index():
     return render_template('index.html')
+
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    rot = request.form['rot']
+    rot = int(rot)
+    text = request.form['text']
+    text = str(text)
+    encrypted_text = rotate_string(text, rot)
+    return render_template('index.html', encrypted_text=encrypted_text)
+
 
 app.run()
